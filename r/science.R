d7a3f36860bab698a6bcadb993a3712404810d93
Young-geun Kim <dudrmshub@gmail.com>
2021-04-28 10:12:41 +0900

    consider the difference as output

--- r/science.R
+++ r/science.R
@@ -3,7 +3,9 @@ library(dplyr)
 library(readr)
 library(tidyr)
 # experiment data------------------
-ecg <- read_csv("data/raw/ecg.csv")
+ecg <- read_csv("data/raw/ecg.csv") %>% 
+  mutate(diff = post - pre) %>% 
+  select(-pre, -post)
 # pre-process----------------------
 science_ecg <- 
   ecg %>% 
@@ -16,16 +18,17 @@ science_ecg <-
       coffee == 4 ~ "W"
     )
   ) %>% 
-  pivot_wider(names_from = trt, values_from = bpm)
+  pivot_wider(names_from = trt, values_from = diff)
 # sharp null of no effect----------
 sharp_null <- 
   science_ecg %>% 
   mutate(
-    HB = ecg$bpm,
-    S = ecg$bpm,
-    De = ecg$bpm,
-    W = ecg$bpm
+    HB = ecg$diff,
+    S = ecg$diff,
+    De = ecg$diff,
+    W = ecg$diff
   )
 # write----------------------------
+write_csv(ecg, "data/processed/ecg-diff.csv")
 write_csv(science_ecg, "data/processed/science.csv")
 write_csv(sharp_null, "data/processed/science-sharp.csv")
