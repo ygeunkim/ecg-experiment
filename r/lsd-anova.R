0d29b00d55ecbdc32a3dbcf524728031ae345b4c
Young-geun Kim <dudrmshub@gmail.com>
2021-05-14 22:11:55 +0900

    finish slide draft

--- r/lsd-anova.R
+++ r/lsd-anova.R
@@ -1,3 +1,47 @@
+# Observed outcomes----------------------------
+# input without useless column e.g. id, date, trt
+gen_table <- function(data, row, col, out, add_mean = FALSE) {
+  latin <- 
+    data %>% 
+    mutate(row = !!ensym(row)) %>% 
+    group_by(row) %>% 
+    mutate(
+      row = str_c("row", !!ensym(row)),
+      col = str_c("col", !!ensym(col))
+    ) %>% 
+    select(-!!ensym(row), -!!ensym(col)) %>% 
+    pivot_wider(names_from = col, values_from = !!ensym(out))
+  if (add_mean) {
+    latin %>% 
+      column_to_rownames(var = "row") %>% 
+      mutate(MEAN = rowMeans(.)) %>% 
+      bind_rows(MEAN = colMeans(.)) %>% 
+      rownames_to_column(var = "row") %>% 
+      mutate(row = str_replace_all(row, pattern = str_c("\\...", nrow(.)), replacement = "MEAN"))
+  } else {
+    latin
+  }
+}
+# print----------------------------------------
+print_obs <- function(tibble, format = "latex", caption = "Observed Outcomes", ...) {
+  tibble %>% 
+    mutate(row = str_remove_all(row, pattern = "^row")) %>% 
+    column_to_rownames(var = "row") %>% 
+    kable(
+      format = format,
+      escape = FALSE,
+      caption = caption,
+      row.names = TRUE,
+      ...
+    ) %>% 
+    kable_styling(full_width = FALSE, latex_options = c("striped", "HOLD_position")) %>% 
+    row_spec(
+      (tibble %>% nrow()), color = "red"
+    ) %>% 
+    column_spec(
+      (tibble %>% ncol()), color = "red"
+    )
+}
 # ANOVA from observed data by LSD--------------
 # input tidy data
 # formula = output ~ row + col + trt
--- r/lsd-rand.R
+++ r/lsd-rand.R
@@ -1,7 +1,13 @@
+# Vectorize (by row) LSD matrix--------------------------
+vec_lsd <- function(x) {
+  x %>% 
+    t() %>% 
+    as.numeric()
+}
 # Hypothesis Test sharp null based on Randomization------
 # Input: imputed science table
 # n > 1
-permute_lsd <- function(n, size, ...) {
+permute_lsd <- function(n, size, fact_name = c("row", "col", "trt"), ...) {
   lsd <- rlatin(n = n, size = size, ...)
   lsd <- 
     lapply(
@@ -22,7 +28,27 @@ permute_lsd <- function(n, size, ...) {
   lsd
 }
 #-------------------------------------------------------
-# FTre for each LSD-------------------------------------
-# Next step: join the observed output
-# calculate FTre
-
+# Hypothesis Test sharp null based on Randomization------
+# x: imputed science table
+# y_obs: Observed output
+# LSD using rlatin
+test_sharp <- function(x, y_obs, B = 1000, size = 4, fact_name = c("row", "col", "trt"), mc_core = 8) {
+  row_obs <- x[[fact_name[1]]]
+  col_obs <- x[[fact_name[2]]]
+  trt_obs <- x[[fact_name[3]]]
+  # permutation----------------------
+  doMC::registerDoMC(cores = mc_core)
+  foreach(b = seq_len(B), .combine = c) %dopar% {
+    rand_trt <- rlatin(1, size = size) %>% vec_lsd()
+    ecg_new <- tibble(
+      row = row_obs,
+      col = col_obs,
+      trt = rand_trt,
+      out = y_obs
+    )
+    aov_lsd(out ~ row + col + trt, data = ecg_new) %>% 
+      broom::tidy() %>% 
+      .$statistic %>% 
+      .[3]
+  }
+}
