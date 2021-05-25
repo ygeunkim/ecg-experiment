86beb0b2a3db6c889f5dd51bfab0d9a6dbf36de3
Young-geun Kim <dudrmshub@gmail.com>
2021-05-25 19:53:34 +0900

    feedback after presentation to try to use log return for heart rate

--- r/science.R
+++ r/science.R
@@ -3,12 +3,15 @@ library(dplyr)
 library(readr)
 library(tidyr)
 # experiment data------------------
-ecg <- read_csv("data/raw/ecg.csv") %>% 
+ecg <- read_csv("data/raw/ecg.csv")
+# diff-----------------------------
+ecg_diff <- 
+  ecg %>% 
   mutate(diff = post - pre) %>% 
   select(-pre, -post)
 # pre-process----------------------
 science_ecg <- 
-  ecg %>% 
+  ecg_diff %>% 
   select(-date) %>% 
   mutate(
     trt = case_when(
@@ -23,12 +26,40 @@ science_ecg <-
 sharp_null <- 
   science_ecg %>% 
   mutate(
-    HB = ecg$diff,
-    S = ecg$diff,
-    De = ecg$diff,
-    W = ecg$diff
+    HB = ecg_diff$diff,
+    S = ecg_diff$diff,
+    De = ecg_diff$diff,
+    W = ecg_diff$diff
   )
 # write----------------------------
-write_csv(ecg, "data/processed/ecg-diff.csv")
+write_csv(ecg_diff, "data/processed/ecg-diff.csv")
 write_csv(science_ecg, "data/processed/science.csv")
 write_csv(sharp_null, "data/processed/science-sharp.csv")
+# log return-----------------------
+ecg_log <- 
+  ecg %>% 
+  mutate_at(vars(pre, post), ~log(.)) %>% 
+  mutate(diff = post - pre) %>% 
+  select(-pre, -post)
+# sharp null-----------------------
+sharp_log <- 
+  ecg_log %>% 
+  select(-date) %>% 
+  mutate(
+    trt = case_when(
+      coffee == 1 ~ "HB",
+      coffee == 2 ~ "S",
+      coffee == 3 ~ "De",
+      coffee == 4 ~ "W"
+    )
+  ) %>%
+  pivot_wider(names_from = trt, values_from = diff) %>% 
+  mutate(
+    HB = ecg_log$diff,
+    S = ecg_log$diff,
+    De = ecg_log$diff,
+    W = ecg_log$diff
+  )
+# write----------------------------
+write_csv(ecg_log, "data/processed/ecg-logreturn.csv")
+write_csv(sharp_log, "data/processed/science-sharp-log.csv")
